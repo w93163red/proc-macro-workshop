f7ef5a780ca894403fd1d4b96c91276bc26f7f44
LinG <w93163red@gmail.com>
2020-05-02 02:13:46 +0000

    partial}

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,30 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let builder = gen_builder(&input.data);
+    println!("{:?}", name);
+    TokenStream::new()
+}
 
+fn gen_builder(data: &Data) -> TokenStream {
+    match *data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => {
+                let recurse = fields.named.iter().map(|f| {
+                    let name = &f.ident;
+                    eprintln!("{:?}", name);
+                });
+            }
+            _ => {}
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    };
     TokenStream::new()
 }
