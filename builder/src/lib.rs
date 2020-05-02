9028b9e85c047a36cfa89dd43723df499448c957
LinG <lingxiang.wang@bytedance.com>
2020-05-01 23:04:22 -0400

    pass test2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,21 +8,24 @@ use syn::{parse_macro_input, Data, DeriveInput, Fields};
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
-    let builder = gen_builder(&input.data);
-    println!("{:?}", name);
-    TokenStream::new()
+    let _builder = gen_builder(&input.data);
+
+    let q = quote! {
+        impl #name {
+           pub fn builder() {}
+        }
+    };
+    eprintln!("{:#?}", q);
+    proc_macro::TokenStream::from(q)
 }
 
 fn gen_builder(data: &Data) -> TokenStream {
     match *data {
         Data::Struct(ref data) => match data.fields {
-            Fields::Named(ref fields) => {
-                let recurse = fields.named.iter().map(|f| {
-                    let name = &f.ident;
-                    eprintln!("{:?}", name);
-                });
-            }
-            _ => {}
+            Fields::Named(ref fields) => {}
+            Fields::Unnamed(ref fields) => {}
+
+            Fields::Unit => {}
         },
         Data::Enum(_) | Data::Union(_) => unimplemented!(),
     };
